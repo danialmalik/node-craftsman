f453fd65eb3933fb8cc5ea073eeaa28b3e3bf34f
danialmalik <danialmalik321@gmail.com>
2020-07-10 11:38:50 +0500

    Chapter 9 mongo

---
+++ Chapter-09-mongo/index1.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect(
+  'mongodb://127.0.0.1:27017/accounting',
+  function (err, connection) {
+    if (err) console.error(err);
+    var collection = connection.collection('customers');
+
+    collection.insert({ 'name': 'Jane Doe' }, function (err, count) {
+
+      collection.find().toArray(function (err, documents) {
+        console.dir(documents);
+        connection.close();
+      });
+
+    });
+
+  });
